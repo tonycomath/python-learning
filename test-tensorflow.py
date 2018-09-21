6b676d38e1d3cffdad1a383270e92e7690cd140a
tonycomath <43462530+tonycomath@users.noreply.github.com>
2018-09-21 13:41:24 +0800

    Create test-tensorflow.py
    Just download some code which show that how to using python coding tensorflow.

---
+++ test-tensorflow.py
@@ -0,0 +1,37 @@
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# 使用 NumPy 生成假数据(phony data), 总共 100 个点.
+x_data = np.float32(np.random.rand(2, 100)) # 随机输入
+y_data = np.dot([0.100, 0.200], x_data) + 0.300
+
+plt.figure()
+plt.plot(x_data)
+plt.plot(y_data)
+plt.show()
+
+
+b = tf.Variable(tf.zeros([1]))
+W = tf.Variable(tf.random_uniform([1, 2], -1.0, 1.0))
+y = tf.matmul(W, x_data) + b
+
+# 最小化方差
+loss = tf.reduce_mean(tf.square(y - y_data))
+optimizer = tf.train.GradientDescentOptimizer(0.5)
+train = optimizer.minimize(loss)
+
+init = tf.initialize_all_variables()
+
+# 启动图 (graph)
+sess = tf.Session()
+sess.run(init)
+
+# 拟合平面
+for step in range(0, 201):
+    sess.run(train)
+    if step % 20 == 0:
+        print('ok')
+      
+print('Finished')
